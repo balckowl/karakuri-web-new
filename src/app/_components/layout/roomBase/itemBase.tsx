54a7810eae7dd07f2b7dd7bf155f04b685c0b5da
takaaki200302@gmail.com <takaaki200302@gmail.com>
2024-04-10 13:57:45 +0900

    add: prob1_3の導線を作りました

--- src/app/_components/layout/roomBase/itemBase.tsx
+++ src/app/_components/layout/roomBase/itemBase.tsx
@@ -1,7 +1,9 @@
 "use client"
 import Image from "next/image"
 import { motion } from "framer-motion";
-import ScrollBar from "~/features/floor1/item/scrollBar";
+import Item1_1 from "~/features/floor1/item/item1_1";
+import Item1_2 from "~/features/floor1/item/item1_3";
+import Item1_3 from "~/features/floor1/item/item1_3";
 
 const ItemBase = ({ currentRoom }: { currentRoom: string }) => {
   // dbに書き換える 
@@ -11,7 +13,6 @@ const ItemBase = ({ currentRoom }: { currentRoom: string }) => {
     <div className="h-full">
       {currentRoom === "bathroom" ?
         <div>
-          {/* scrollbar */}
           <div>
             <motion.div
               animate={{ opacity: [0, 1] }}
@@ -19,7 +20,23 @@ const ItemBase = ({ currentRoom }: { currentRoom: string }) => {
               className="absolute left-1/2 top-1/2 size-4/5 max-w-[1000px] -translate-x-1/2 -translate-y-1/2 rounded-[30px] bg-white shadow-lg"
             >
               <div className="h-full p-6">
-                <ScrollBar />
+                <Item1_1 />
+              </div>
+            </motion.div>
+          </div>
+
+          <Image src="/images/floor1/rooms/bathroom.webp" alt="bathroom" width="2000" height="2000" className="absolute left-0 top-0 -z-10 h-screen w-full object-cover"/>
+        </div>
+        : currentRoom === "storeroom" ?
+        <div>
+          <div>
+            <motion.div
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              className="absolute left-1/2 top-1/2 size-4/5 max-w-[1000px] -translate-x-1/2 -translate-y-1/2 rounded-[30px] bg-white shadow-lg"
+            >
+              <div className="h-full p-6">
+                <Item1_3 />
               </div>
             </motion.div>
           </div>
---
+++ src/features/floor1/item/item1_3.tsx
@@ -0,0 +1,51 @@
+"use client"
+import Image from "next/image";
+import { useState } from "react";
+import GetItemPopup from "~/app/_components/elements/getItemPopUp/getItemPopUp";
+import { usePlayerDataStore } from "~/store/playerDataStore";
+
+const Item1_3 = () => {
+  const { playerData, setPlayerData } = usePlayerDataStore();
+  const preBelongings: string[] = playerData.belongingList;
+  const [isGetItem, setIsGetItem] = useState<boolean>(false);
+
+  const getScrollBar = () => {
+    setIsGetItem(true);
+    // アイテムを取った判定 & 持ち物の追加 & 社交室の会話フラグ
+    setPlayerData(
+      {
+        isGetItems: { 
+          scrollBar: false,
+          lightToggle: true,
+        },
+        belongingList: [...preBelongings, "lightToggle"],
+        socialroom: {
+          eventIndex: 1,
+          event0Finished: true
+        }
+      }
+    )
+  }
+
+  return (
+    <div className="flex size-full items-center justify-center">
+      {isGetItem &&
+        <GetItemPopup>
+          <p className="mb-2 text-center">アイテムを入手しました</p>
+          <div className="flex h-[200px] w-[300px] flex-col items-center justify-center">
+            <Image src={"/images/floor1/item/scroll.png"} width={200} height={100} alt="scroll" className="mb-2 h-[30px]"></Image>
+            <p>スクロールバー</p>
+          </div>
+        </GetItemPopup>
+      }
+
+      <div onClick={getScrollBar} className="cursor-pointer">
+        {playerData.isGetItems.lightToggle == false &&
+          <Image src={"/images/floor1/item/scroll.png"} width={200} height={100} alt="scroll" className="mb-2 h-[30px]"></Image>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Item1_3

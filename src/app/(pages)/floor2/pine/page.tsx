a78b231a21505cb2e95436d31599236838af8425
takaaki200302@gmail.com <takaaki200302@gmail.com>
2024-04-15 17:36:35 +0900

    add : 部屋ページを作成しました。

---
+++ src/app/(pages)/floor2/pine/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { useEffect } from "react"
+import Belongings from "~/app/_components/elements/belongings/belongings"
+import Floor2Map from "~/app/_components/elements/floormap/floor2/floor2Map"
+import { usePlayerDataStore } from "~/store/playerDataStore"
+
+const Corridor = () => {
+  const { setPlayerData } = usePlayerDataStore();
+  // 現在位置の更新
+  useEffect(() => {
+    setPlayerData({ currentRoom: "pine" })
+  }, [setPlayerData])
+
+  return (
+    <div>
+
+
+      <Floor2Map />
+      <Belongings />
+    </div>
+  )
+}
+
+export default Corridor

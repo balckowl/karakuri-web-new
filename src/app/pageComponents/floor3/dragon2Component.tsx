763e17414c45b9bf051e30a66f3efde7e68fa8d1
takaaki200302@gmail.com <takaaki200302@gmail.com>
2024-04-23 14:06:20 +0900

    add: 迷路ギミックを作成中...

--- src/app/pageComponents/floor3/dragon2Component.tsx
+++ src/app/pageComponents/floor3/dragon2Component.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import Belongings from "~/app/_components/elements/belongings/belongings";
 import Floor3Map from "~/app/_components/elements/floormap/floor3/floor3Map";
 import DownArrow from "~/app/_components/elements/roomChangeArrow/downArrow/downArrow";
+import ProbBase from "~/app/_components/layout/roomBase/probBase";
 import { usePlayerDataStore } from "~/store/playerDataStore"
 
 const Dragon2Component = () => {
@@ -15,6 +16,8 @@ const Dragon2Component = () => {
   return (
     <div>
       <DownArrow  floor={3} hrefProps={"dragon1"}/>
+
+      <ProbBase currentRoom={"dragon2"} />
 
       <Floor3Map />
       <Belongings />
---
+++ src/features/prob/prob3_1.tsx
@@ -0,0 +1,124 @@
+import { useEffect, useState } from "react";
+
+
+const Prob3_1 = () => {
+  function deepCopy2DArray(arr: any) {
+    return arr.map(function (subArray: any) {
+      return Array.isArray(subArray) ? deepCopy2DArray(subArray) : subArray;
+    });
+  }
+
+  const isvalid = (i: number, j: number) => {
+    if (0 <= i && i < gridSize && 0 <= j && j < gridSize) {
+      if (grid[i][j] != "#") {
+        return true
+      }
+    }
+    return false
+  }
+
+  const handleMove = (operate: string) => {
+    let x = playerX
+    let y = playerY
+    let dir = direction
+    if (operate == "U") {
+      if (isvalid(playerX - 1, playerY)) {
+        x -= 1
+        dir = "U"
+      }
+    } else if (operate == "D") {
+      if (isvalid(playerX + 1, playerY)) {
+        x += 1
+        dir = "D"
+      }
+    } else if (operate == "L") {
+      if (isvalid(playerX, playerY - 1)) {
+        y -= 1
+        dir = "L"
+      }
+    } else if (operate == "R") {
+      if (isvalid(playerX, playerY + 1)) {
+        y += 1
+        dir = "R"
+      }
+    }
+    const newGrid = deepCopy2DArray(grid)
+    newGrid[playerX][playerY] = "."
+    newGrid[x][y] = dir
+    newGrid[2][2] = "G"
+    setGrid(newGrid)
+    setPlayerX(x)
+    setPlayerY(y)
+    setDirection(dir)
+    if (x == 0 && y == 3) {
+      console.log("goal")
+    }
+  }
+
+  useEffect(() => {
+    const newGrid = deepCopy2DArray(grid)
+    newGrid[0][0] = "R"
+    setGrid(newGrid)
+  }, []);
+
+  const gridSize = 5
+  const [playerX, setPlayerX] = useState<number>(0);
+  const [playerY, setPlayerY] = useState<number>(0);
+  const [direction, setDirection] = useState<string>("R");
+  const [grid, setGrid] = useState<string[][]>(
+    [
+      [".", ".", ".", ".", "."],
+      [".", "#", "#", "#", "."],
+      [".", "#", "G", "#", "."],
+      [".", "#", "@", "#", "."],
+      [".", ".", ".", ".", "."],
+    ]
+  );
+
+  return (
+    <div className="relative size-full">
+      <div className="flex h-full items-center flex-col mx-auto justify-center">
+        <div className="mx-auto outline mb-12">
+          {grid.map((mass, index) => (
+            <div key={index} className="flex">
+              {mass.map((item, index) => (
+                <div
+                  key={index}
+                  className={`w-[90px] h-[90px] border-black dark:border-white border-[1px] ${item == "#" && "bg-black dark:bg-white"}`}
+                >
+                  <p className="h-full flex items-center justify-center text-4xl font-bold">
+                    {"URDLG".includes(item) && item}
+                  </p>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* 操作盤 */}
+      <div className="absolute right-10 bottom-2">
+        <div className="flex justify-center w-full">
+          <kbd
+            onClick={()=>handleMove("U")}
+            className="kbd cursor-pointer select-none">▲</kbd>
+        </div>
+        <div className="flex justify-center gap-12 w-full">
+          <kbd
+            onClick={()=>handleMove("L")}
+            className="kbd cursor-pointer select-none">◀︎</kbd>
+          <kbd
+            onClick={()=>handleMove("R")}
+            className="kbd cursor-pointer select-none">▶︎</kbd>
+        </div>
+        <div className="flex justify-center w-full">
+          <kbd
+            onClick={()=>handleMove("D")}
+            className="kbd cursor-pointer select-none">▼</kbd>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Prob3_1

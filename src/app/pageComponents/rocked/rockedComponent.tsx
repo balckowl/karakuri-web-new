198504bb367b91c8f75d597e6579177a1c1509bf
DESKTOP-8B8N2FK\tyamaguchi <takaaki200302@gmail.com>
2024-04-30 16:16:53 +0900

    adc: rockedの画像を作成しました。

--- src/app/pageComponents/rocked/rockedComponent.tsx
+++ src/app/pageComponents/rocked/rockedComponent.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Image from "next/image";
 
 const RockedComponent = () => {
+
   return (
-    <div>RockedComponent</div>
+    <div>
+      <Image src="/images/other/rocked.webp" alt="rocked" width="2000" height="2000" className="absolute left-0 top-0 -z-10 h-screen w-full object-cover" />
+    </div>
   )
 }
 
